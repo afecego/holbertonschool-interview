bb979a61c1bce23687283853008bcdf1d8736e3d
afecego <afecego@gmail.com>
2022-05-01 12:00:03 -0500

    Change conditional

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,10 +8,11 @@ def minOperations(n):
     operations needed to result in exactly n H characters in the file."""
 
     result = 0
-    if n <= 1:
+    if n <= 0:
         return 0
     else:
-        for i in range(2, n + 1):
+        end = n + 1
+        for i in range(2, end):
             while n % i == 0:
                 n = n / i
                 result += i
