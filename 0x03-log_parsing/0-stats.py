4756558096e6240b4e76492620901104191f2381
afecego <afecego@gmail.com>
2022-05-06 00:12:23 -0500

    Correction spaces

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
 import sys
 
-code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-num_line = 1
-size = [0]
 
-for line in sys.stdin:
-    line = line.rstrip()
-    posc = line.split(' ')
-    get_code = int(posc[7])
-    size[0] += int(posc[8])
-    if get_code in code:
-        code[get_code] += 1
-    if num_line % 10 == 0:
-        print('File size: {}'.format(size[0]))
-        for key in sorted(code.keys()):
-            if code[key]:
-                print('{}: {}'.format(key, code[key]))
-    num_line += 1
+def stats():
+    """Write a script that reads stdin line by line and computes metrics:"""
+    code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    num_line = 1
+    size = [0]
+
+    for line in sys.stdin:
+        line = line.rstrip()
+        posc = line.split(' ')
+        get_code = int(posc[7])
+        size[0] += int(posc[8])
+        if get_code in code:
+            code[get_code] += 1
+        if num_line % 10 == 0:
+            print('File size: {}'.format(size[0]))
+            for key in sorted(code.keys()):
+                if code[key]:
+                    print('{}: {}'.format(key, code[key]))
+        num_line += 1
+
+
+if __name__ == '__main__':
+    """Call the main function"""
+    stats()
