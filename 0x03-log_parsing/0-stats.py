5ab67e668ee5b7eadd38527c5c4e266e532d6176
afecego <afecego@gmail.com>
2022-05-06 00:23:04 -0500

    Correction spaces

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""Write a script that reads stdin line by line and computes metrics"""
 import sys
 
 
 def stats():
-    """Write a script that reads stdin line by line and computes metrics:"""
+    """print these statistics from the beginning:
+    Total file size: File size: <total size>
+    where <total size> is the sum of all previous <file size>
+    Number of lines by status code:
+    * possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+    * if a status code doesn’t appear or is not an integer, don’t print
+    anything for this status code
+    * format: <status code>: <number>
+    * status codes should be printed in ascending order"""
     code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     num_line = 1
     size = [0]
