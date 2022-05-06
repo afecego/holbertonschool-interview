f2d35674330f2375a8604453124ea743b7ecb542
afecego <afecego@gmail.com>
2022-05-05 23:52:40 -0500

    List of stats

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+
+code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+num_line = 1
+size = [0]
+
+for line in sys.stdin:
+    line = line.rstrip()
+    posc = line.split(' ')
+    get_code = int(posc[7])
+    size[0] += int(posc[8])
+    if get_code in code:
+        code[get_code] += 1
+    if num_line % 10 == 0:
+        print('File size: {}'.format(size[0]))
+        for key in sorted(code.keys()):
+            if code[key]:
+                print('{}: {}'.format(key, code[key]))
+    num_line += 1
