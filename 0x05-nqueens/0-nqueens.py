e72d68716389cd070346ff1aceb73c9e2ca27f34
afecego <afecego@gmail.com>
2022-05-22 23:27:45 -0500

    nqueens puzzle

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,3 +4,39 @@ on an N×N chessboard"""
 import sys
 
 
+def nonAttack(sol, ln, i):
+    for j in range(ln):
+        if(sol[j] == i or sol[j] + ln - j == i or sol[j] + j - ln == i):
+            return False
+    return True
+
+
+def nextSafe(sol, ln):
+    for i in range(len(sol)):
+        if nonAttack(sol, ln, i):
+            sol[ln] = i
+            if ln < len(sol) - 1:
+                nextSafe(sol, ln + 1)
+            else:
+                print([[i, sol[i]] for i in range(len(sol))])
+
+if len(sys.argv) != 2:
+    print ('Usage: nqueens N\n')
+    sys.exit(1)
+else:
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number\n')
+        sys.exit(1)
+    if type(n) is not int:
+        print('N must be a number\n')
+        sys.exit(1)
+    if n < 4:
+        print ('N must be at least 4\n')
+        sys.exit(1)
+    else:
+        sol = []
+        for i in range(n):
+            sol.append(i)
+        nextSafe(sol, 0)
