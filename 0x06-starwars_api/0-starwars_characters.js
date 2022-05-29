579c444395d95b6dc0c91b3b545c8bbcf7bb3324
afecego <afecego@gmail.com>
2022-05-29 18:13:02 -0500

    correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,32 @@
 #!/usr/bin/node
 // script that prints all characters of a Star Wars movie
 const request = require('request');
-const ac = process.argv.slice(2);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request.get(`https://swapi-api.hbtn.io/api/films/${ac}/`, async (_error, response, body) => {
-  const characters = JSON.parse(body).characters;
-  for (const character of characters) {
-    const charac = await new Promise((resolve, reject) => {
-      request(character, (errors, responses, bodys) => {
-        if (errors) {
-          reject(errors);
-        } else {
-          resolve(JSON.parse(bodys).name);
-        }
-      });
-    });
-    console.log(charac);
+function getCharsList (_, __, body) {
+  const info = JSON.parse(body).characters;
+  handleCharsList(info);
+}
+
+request(url, getCharsList);
+
+function handleCharsList (value) {
+  const arr = [];
+
+  const printCharsList = (value2, idx, charCount) => {
+    arr.push([value2, idx]);
+    if (arr.length === charCount) {
+      const newArr = arr.sort((a, b) => a[1] - b[1]);
+      for (let i = 0; i < newArr.length; i++) {
+        console.log(newArr[i][0]);
+      }
+    }
+  };
+  for (let i = 0; i < value.length; i++) {
+    const handleCharsName = (_, __, body) => {
+      const info = JSON.parse(body).name;
+      printCharsList(info, i, value.length);
+    };
+    request(value[i], handleCharsName);
   }
-});
+}
