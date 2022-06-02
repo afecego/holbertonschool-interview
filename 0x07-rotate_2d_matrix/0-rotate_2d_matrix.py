ee950339f2c1adb2676a68289f85f26293a48787
afecego <afecego@gmail.com>
2022-06-02 15:10:12 -0500

    Correction creation of list

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,6 +5,7 @@
 def rotate_2d_matrix(matrix):
     """The matrix must be edited in-place"""
     row = len(matrix)
+    temp = []
 
     for j in range(row):
         col_row = []
@@ -12,4 +13,9 @@ def rotate_2d_matrix(matrix):
             a = i[j]
             col_row.append(a)
         col_row.reverse()
-        matrix[j] = col_row
+        print(col_row)
+        temp.append(col_row)
+    print(temp)
+    for i in range(row):
+        for j in range(row):
+            matrix[i][j] = temp[i][j]
