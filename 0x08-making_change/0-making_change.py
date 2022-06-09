e47abe134f3e1d89bfcbfc9872ee0e1208c35f58
afecego <afecego@gmail.com>
2022-06-09 00:34:06 -0500

    few number of coins needed to meet total

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""determine the fewest number of coins needed to meet a given amount total"""
+
+
+def makeChange(coins, total):
+    """Return: fewest number of coins needed to meet total"""
+    if total <= 0:
+        return 0
+
+    if coins is None:
+        return -1
+
+    new_coins = []
+    coins.sort(reverse=True)
+    for i in coins:
+        new_coins.append(i)
+
+    new = 0
+    for i in new_coins:
+        if total / i <= total:
+            new += int(total / i)
+            total = total % i
+    if total == 0:
+        return new
+    return -1
