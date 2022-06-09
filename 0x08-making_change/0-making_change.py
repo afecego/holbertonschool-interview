7565467c3b53ebd8be3edb8f3e1bb5bbc9441244
afecego <afecego@gmail.com>
2022-06-09 09:30:07 -0500

    fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """determine the fewest number of coins needed to meet a given
 amount total"""
-from typing import List
 
 
-def makeChange(coins: List[int], total: int) -> int:
+def makeChange(coins, total: int) -> int:
     """Return: fewest number of coins needed to meet total"""
     if total <= 0:
         return 0
