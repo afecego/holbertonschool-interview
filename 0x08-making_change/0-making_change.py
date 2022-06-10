ef43cb3f8318fa44457322503850c097755ab12b
afecego <afecego@gmail.com>
2022-06-10 00:27:02 -0500

    fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,7 +4,7 @@
 
 def makeChange(coins, total):
     """Return fewest number of coins needed to meet total"""
-    if total <= 0:
+    if total < 0:
         return 0
 
     coins.sort(reverse=True)
