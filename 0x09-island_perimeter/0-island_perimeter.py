fede6cb61f9c1f2090875dfdd0308896728423f0
afecego <afecego@gmail.com>
2022-06-13 19:13:15 -0500

    Perimeter of a fig in a matrix

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """grid is a list of list of integers:
+    0 represents water
+    1 represents land
+    Each cell is square, with a side length of 1
+    Cells are connected horizontally/vertically (not diagonally).
+    grid is rectangular, with its width and height not exceeding 100"""
+
+    if len(grid) > 100 or len(grid[0]) > 100:
+        return ("Error")
+
+    count = 0
+
+    print(range(len(grid)))
+    print(range(len(grid[0])))
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            print(i)
+            if grid[i][j] == 1:
+                if grid[i - 1][j] != 1:
+                    count += 1
+
+                if grid[i][j - 1] != 1:
+                    count += 1
+
+                if j + 1 > len(grid[i]) - 1 or grid[i][j + 1] != 1:
+                    count += 1
+
+                if i + 1 > len(grid) - 1 or grid[i + 1][j] != 1:
+                    count += 1
+
+    return count
