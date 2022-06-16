201a17edd401c46828ea230117d5456c7f0d027c
afecego <afecego@gmail.com>
2022-06-15 23:48:07 -0500

    fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -16,10 +16,10 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
 
-                if grid[i - 1][j] != 1:
+                if i - 1 < 0 or grid[i - 1][j] != 1:
                     count += 1
 
-                if grid[i][j - 1] != 1:
+                if j - 1 < 0 or grid[i][j - 1] != 1:
                     count += 1
 
                 if j + 1 > len(grid[i]) - 1 or grid[i][j + 1] != 1:
